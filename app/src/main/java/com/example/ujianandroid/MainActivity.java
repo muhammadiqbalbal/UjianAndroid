7616577daf8e72e5acbff270bcfa23a158abf5ab
ASUS <muhammadiqbalbal1605@gmail.com>
2023-07-10 22:24:10 +0700

    UAS Muhammad Iqbal

--- app/src/main/java/com/example/ujianandroid/MainActivity.java
+++ app/src/main/java/com/example/ujianandroid/MainActivity.java
@@ -42,7 +42,23 @@ public class MainActivity extends AppCompatActivity {
                     daftar_nama.clear();
 
                     for (int i = 0; i < doubleisian_nama_umur; i++) {
-                        daftar_nama.add(nama_lengkap);
+                        String status;
+
+                        if (doubleisian_nama_umur >= 0 && doubleisian_nama_umur <= 10) {
+                            status = "anak";
+                        } else if (doubleisian_nama_umur >= 11 && doubleisian_nama_umur <= 20) {
+                            status = "remaja";
+                        } else if (doubleisian_nama_umur >= 21 && doubleisian_nama_umur <= 30) {
+                            status = "dewasa";
+                        } else {
+                            status = "tua";
+                        }
+
+                        if (i % 2 == 0) {
+
+                            String nama_dengan_status = i + " " + nama_lengkap + ". Status : " + status;
+                            daftar_nama.add(nama_dengan_status);
+                        }
                     }
 
                     edNamaDepan.setText("");
